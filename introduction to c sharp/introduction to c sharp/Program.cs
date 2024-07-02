4d5bb9325201bed0f1b6e2d8078ce1cc17ebe98a
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-03 02:20:15 +0530

    program.cs

---
+++ introduction to c sharp/introduction to c sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace introduction_to_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to c sharp");
+            Console.WriteLine("learn c sharp");
+            Console.WriteLine();
+        }
+    }
+}
