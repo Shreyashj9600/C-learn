2f3e6997ce78e7eecce828b9c9f05c5c1a18e352
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-05 01:58:41 +0530

    realtional operator

---
+++ 03_statements/statements/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            bool c = a == b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
+    
