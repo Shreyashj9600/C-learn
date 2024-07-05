1482888b348c32cb9ed9114be0309382c2a63631
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-05 19:53:22 +0530

    for loop

---
+++ 05_table in loop/table in loop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace table_in_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + "x" + i + "=" + number * i);
+            }
+            Console.WriteLine("end the loop");
+            Console.ReadLine();
+        }
+    }
+}
